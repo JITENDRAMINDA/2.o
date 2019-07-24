309100be9bfe865185edf9d4b8e9e14670b9b8d0
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-24 17:13:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,13 +64,11 @@ def main(client, message):
     x = c.split()
     id = str(message.message_id)
     if id in x:
-     try:
       if messege.text == ".":
        client.delete_messages(int(m),int(x[x.index(id)+1]))
       else:
        client.edit_message_text(int(m),int(x[x.index(id)+1]), "**" + message.text + "**" )
-     except:
-      continue
+     
 @app.on_message(Filters.chat(bullet) & Filters.edited)
 def main(client, message):
  file = open("bullet.txt" , "r")
@@ -86,14 +84,11 @@ def main(client, message):
     x = c.split()
     id = str(message.message_id)
     if id in x:
-     try:
       if messege.text == ".":
        client.delete_messages(int(m),int(x[x.index(id)+1]))
       else:
        client.edit_message_text(int(m),int(x[x.index(id)+1]), "**" + message.text + "**" )
-     except:
-      continue
-
+     
 
 @app.on_message(Filters.command('add') & Filters.user(491634139) )
 def forward(client, message):
