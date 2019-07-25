5a7dde20e38f11442d0915aa99f79e9bda72f686
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-25 13:29:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -157,13 +157,13 @@ def forward(client, message):
 @app.on_message(Filters.command('reset') & Filters.user(491634139))
 def forward(client, message):
  with open("ferrari.txt" , "w") as g:
-  g.write("001")
+  g.write("")
   g.close()
  with open("bullet.txt" , "w") as g:
-  g.write("001")
+  g.write("")
   g.close()
  with open("ids.txt" , "w") as g:
-  g.write("001")
+  g.write("")
   g.close()
  message.reply("done")
 
