0a9274ebd638db4dc454b09074c62c63fe357eab
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-11-09 16:05:30 +0530

    Update su.py

--- su.py
+++ su.py
@@ -19,10 +19,7 @@ def main(client, message):
    p = line.split()
    for s in p:
     try:
-     mes = client.send_message( int(s), "**" + message.text + "**" )
-     fie = open(str(s)+".txt","a")
-     fie.write(" " + str(message.message_id) + " " + str(mes.message_id))
-     fie.close()
+     client.send_message( int(s), "**" + message.text + "**" )
     except:
      continue
 @app.on_message(Filters.chat(ferrari) & ~ Filters.edited)
@@ -40,56 +37,9 @@ def main(client, message):
    p = line.split()
    for r in p: 
     try:
-     mes = client.send_message( int(r), "**" + message.text + "**" )
-     fie = open(str(r)+".txt","a")
-     fie.write(" " + str(message.message_id) + " " + str(mes.message_id))
-     fie.close()
+     client.send_message( int(r), "**" + message.text + "**" )
     except:
      continue
-@app.on_message(Filters.chat(ferrari) & Filters.edited)
-def main(client, message):
- file = open("ferrari.txt" , "r")
- lines = file.readlines()
- file.close()
- for line in lines:
-  t = line.split()
-  for m in t:
-   files = open(str(m)+".txt" , "r")
-   d = files.readlines()
-   files.close()
-   for c in d:
-    x = c.split()
-    id = str(message.message_id)
-    if id in x:
-     try:
-      if message.text == ".":
-       client.delete_messages(int(m),int(x[x.index(id)+1]))
-      else:
-       client.edit_message_text(int(m),int(x[x.index(id)+1]), "**" + message.text + "**" )
-     except FloodWait as e:
-       time.sleep(e.x)
-@app.on_message(Filters.chat(bullet) & Filters.edited)
-def main(client, message):
- file = open("bullet.txt" , "r")
- lines = file.readlines()
- file.close()
- for line in lines:
-  b = line.split()
-  for a in b:
-   files = open(str(a)+".txt" , "r")
-   d = files.readlines()
-   files.close()
-   for c in d:
-    x = c.split()
-    id = str(message.message_id)
-    if id in x:
-     try:
-      if message.text == ".":
-       client.delete_messages(int(a),int(x[x.index(id)+1]))
-      else:
-       client.edit_message_text(int(a),int(x[x.index(id)+1]), "**" + message.text + "**" )
-     except FloodWait as e:
-      time.sleep(e.x)
 @app.on_message(Filters.command('add') & Filters.user(491634139) )
 def forward(client, message):
  if len(message.text.split(' ')) > 2:
@@ -128,35 +78,23 @@ def forward(client, message):
     message.reply("💼 Please write a valid chat id. ✅✅ ")
  else:
     message.reply("💼 Please write a valid chat id. ✅✅ ")
-@app.on_message(Filters.command('clear') & Filters.user(491634139))
-def forward(client, message):
-    file = open(message.text.split(" ")[1] + ".txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-     p = line.split()
-     for x in p:
-        fie = open(str(x)+".txt","w")
-        fie.write("001 002")
-        fie.close()
-        message.reply("☢️ Done, Editing data cleared ✅✅")
 @app.on_message(Filters.command('list') & Filters.user(491634139))
 def forward(client, message):
   file = open(message.text.split(" ")[1] + ".txt" , "r")
   u = file.readlines()
   file.close()
   for v in u :
-      message.reply("🏘️ List of Chat_ids in my database are ```" + str(v) + "```. Its can be change. ✅✅")
+   message.reply("🏘️ List of Chat_ids in my database are ```" + str(v) + "```. Its can be change. ✅✅")
 @app.on_message(Filters.command('get') & Filters.user(491634139) )
 def forward(client, message):
  if len(message.text.split(' ')) > 1:
   if len(message.text.split(' ')[1]) == 14:
-      x = client.get_chat(int(message.text.split(' ')[1])).title
-      message.reply("📶 This chat name is - "+str(x)+" ✅")
+   x = client.get_chat(int(message.text.split(' ')[1])).title
+   message.reply("📶 This chat name is - "+str(x)+" ✅")
   else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
+   message.reply("💼 Please write a valid chat id. ✅✅ ")
  else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
+  message.reply("💼 Please write a valid chat id. ✅✅ ")
 
 @app.on_message(Filters.command('reset') & Filters.user(491634139))
 def forward(client, message):
