119fce6ba46e9d3190b7202fe94102ecc07696d7
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-11-09 14:43:07 +0530

    Update su.py

--- su.py
+++ su.py
@@ -1,1 +1,6 @@
-dd
+ f = False
+ words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
+ for word in words:
+  if word.casefold() in Message.text.casefold():
+   f = True
+ if not f:
